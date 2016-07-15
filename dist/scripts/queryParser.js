f2cb05a2f81bb23f822cfd2280f89372ced94b30
田杰 <jefftian@hcdlearning.com>
2016-07-15 10:59:25 +0800

    generate all.min.js

--- dist/scripts/queryParser.js
+++ dist/scripts/queryParser.js
@@ -1,1 +1,58 @@
-angular.module("angularQueryParserModule").factory("queryParser",[function(){var r={getQueryString:function(){return window.location.search||window.location.hash}};return r.get=function(e,n){1===arguments.length&&(n=e,e=r.getQueryString());var t=e.search(new RegExp(n,"i"));if(e[t-1]&&"?"!==e[t-1]&&"&"!==e[t-1]&&(t=-1),t>=0){var u=e.indexOf("&",t+n.length);return 0>u&&(u=e.length),e.substring(t+n.length+1,u)}return""},r.parse=function(e){0===arguments.length&&(e=e||r.getQueryString()),e=e.substr(1);for(var n=e.split("&"),t={},u=0;u<n.length;u++){var a=n[u];if(a){var i=a.split("=");t[i[0]]=decodeURIComponent(i[1])}}return t},r}]);
+angular.module('angularQueryParserModule')
+    .factory('queryParser', [function () {
+        var q = {
+            getQueryString: function () {
+                return window.location.search || window.location.hash;
+            }
+        };
+
+        q.get = function (query, key) {
+            if (arguments.length === 1) {
+                key = query;
+                query = q.getQueryString();
+            }
+
+            var index = query.search(new RegExp(key, 'i'));
+
+            if (query[index - 1] && query[index - 1] !== '?' && query[index - 1] !== '&') {
+                index = -1;
+            }
+
+            if (index >= 0) {
+                var end = query.indexOf('&', index + key.length);
+
+                if (end < 0) {
+                    end = query.length;
+                }
+
+                return query.substring(index + key.length + 1, end);
+            } else {
+                return '';
+            }
+        };
+
+        q.parse = function (query) {
+            if (arguments.length === 0) {
+                query = query || q.getQueryString();
+            }
+
+            query = query.substr(1);
+
+            var pairs = query.split('&');
+            var result = {};
+
+            for (var i = 0; i < pairs.length; i++) {
+                var pair = pairs[i];
+
+                if (pair) {
+                    var parts = pair.split('=');
+
+                    result[parts[0]] = decodeURIComponent(parts[1]);
+                }
+            }
+
+            return result;
+        };
+
+        return q;
+    }]);
